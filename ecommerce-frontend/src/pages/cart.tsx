b8eef006929ab01c5e64ae451f6ef67184c6ef59
Mohit <mohit.yadav@wealthy.in>
2024-04-05 15:31:26 +0530

    Cart

--- ecommerce-frontend/src/pages/cart.tsx
+++ ecommerce-frontend/src/pages/cart.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
 const Cart = () => {
   return (
-    <div>
-      Cart
+    <div className="cart">
+      <main></main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em className="red"> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+      </aside>
     </div>
   )
 }
