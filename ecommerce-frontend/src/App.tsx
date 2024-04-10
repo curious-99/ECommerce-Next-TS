db17b0e79839a6f8430b88a240843ef2c0db0d27
curious-99 <mohit39.ugcs20@iiitranchi.ac.in>
2024-04-10 16:57:58 +0530

    add orders page

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -8,6 +8,7 @@ const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
 const Shipping = lazy(() => import("./pages/shipping"));
 const Login = lazy(() => import("./pages/login"));
+const Orders = lazy(() => import("./pages/orders"));
 
 //Admin Routes Importing
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -46,6 +47,7 @@ const App = () => {
       {/* Logged in User Routes */}
       <Route>
         <Route path="/shipping" element={<Shipping />} />
+        <Route path="/orders" element={<Orders />} />
       </Route>
 
 
